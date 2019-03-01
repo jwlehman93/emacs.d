fd43255e22058759747d6c8d8be5ea100f8380e2
Jeremy Lehman <jerleh@amazon.com>
2019-03-01 14:02:10 -0800

    Update docker to use-package and add keybinding

--- lisp/init-docker.el
+++ lisp/init-docker.el
@@ -2,15 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'docker)
+(use-package docker
+  :ensure t
+  :init
   (fullframe docker-images tablist-quit)
   (fullframe docker-machines tablist-quit)
   (fullframe docker-volumes tablist-quit)
   (fullframe docker-networks tablist-quit)
-  (fullframe docker-containers tablist-quit))
-(maybe-require-package 'dockerfile-mode)
-(maybe-require-package 'docker-compose-mode)
-
+  (fullframe docker-containers tablist-quit)
+  :bind ("C-c d" . docker)
+  :config
+  (use-package dockerfile-mode :ensure t)
+  (use-package docker-compose-mode :ensure t))
 
 (provide 'init-docker)
 ;;; init-docker.el ends here
