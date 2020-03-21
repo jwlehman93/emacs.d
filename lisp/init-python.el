4dd514fc5b225a5de2f21b3e36a0d9a4370aea24
Jeremy Lehman <jeremylehman9@gmail.com>
2020-03-21 14:33:16 -0700

    Add special goto function for bad finds

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,13 +6,23 @@
       (append '(("SConstruct\\'" . python-mode)
                 ("SConscript\\'" . python-mode))
               auto-mode-alist))
-
 (require-package 'pip-requirements)
 
 (use-package elpy
   :ensure t
   :init
-  (elpy-enable))
+  (elpy-enable)
+  (define-key elpy-mode-map (kbd "M-.") 'elpy-goto-definition-or-rgrep))
+
+(defun elpy-goto-definition-or-rgrep ()
+  "Go to the definition of the symbol at point, if found.  Otherwise, run `elpy-rgrep-symbol'."
+  (interactive)
+  (ring-insert find-tag-marker-ring (point-marker))
+  (condition-case nil (elpy-goto-definition)
+    (error (elpy-rgrep-symbol
+            (concat "\\(def\\|class\\)\s" (thing-at-point 'symbol) "(")))))
+
+
 
 (provide 'init-python)
 ;;; init-python.el ends here
